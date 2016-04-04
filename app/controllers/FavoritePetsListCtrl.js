665008b9f5a937d8b8c637ca34e1e248636bb3d4
Kaylee Cummings <kayleecummings@Kaylees-MacBook-Pro.local>
2016-04-04 17:08:38 -0500

    styling is almost done

--- app/controllers/FavoritePetsListCtrl.js
+++ app/controllers/FavoritePetsListCtrl.js
@@ -26,7 +26,19 @@ App.controller("FavoritePetsListCtrl",
         //   $scope.favoritePets.push(userCollection[key]);
         // });
 
-////////////here
+        // function chunk(arr, size) {
+        //   var newArr = [];
+        //   for (var i=0; i<arr.length; i+=size) {
+        //     newArr.push(arr.slice(i, i+size));
+        //   }
+        //   return newArr;
+        // }
+
+        // $scope.chunkedData = chunk(myData, 3);
+        
+        // console.log("chunkedData", $scope.chunkedData);
+        // console.log("newArr", newArr);
+
 
         for (let key in userCollection) {
           $scope.favoritePets.push(userCollection[key]);
